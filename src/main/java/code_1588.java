9464d9c863d67480c3243eff289ecc93dfd89945
ZNW_Mac <244407031@qq.com>
2020-12-25 15:04:15 +0800

    leetcode_1588

---
+++ src/main/java/code_1588.java
@@ -0,0 +1,16 @@
+public class code_1588 {
+    public int sumOddLengthSubarrays(int[] arr) {
+        int num = 0;
+        for (int i = 0; i < arr.length; i++) {
+            int left = i + 1;
+            int right = arr.length - i;
+            int left_e = (left + 1) / 2;
+            int right_e = (right + 1) / 2;
+            int left_o = left / 2;
+            int right_o = right / 2;
+            num += (left_e * right_e + left_o * right_o)  * arr[i];
+        }
+        return num;
+    }
+    
+}
