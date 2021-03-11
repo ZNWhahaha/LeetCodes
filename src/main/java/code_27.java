7843029e0a914f35c7b69964c2063b228cc00296
ZNW_Mac <244407031@qq.com>
2021-03-11 16:19:02 +0800

    code_15

--- src/main/java/code_27.java
+++ src/main/java/code_27.java
@@ -2,11 +2,9 @@ public class code_27 {
     public int removeElement(int[] nums, int val) {
         int lowIndex = 0;
         for (int fastIndex = 0; fastIndex < nums.length; fastIndex++) {
-            if (nums[fastIndex] == val){
-                continue;
+            if (nums[fastIndex] != val){
+                nums[lowIndex++] = nums[fastIndex];
             }
-            lowIndex++;
-            nums[lowIndex] = nums[fastIndex];
         }
         return lowIndex;
     }
