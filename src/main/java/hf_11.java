18f0eab0c944215f794a18d87ee7196ed437f50b
ZNW_Mac <244407031@qq.com>
2021-03-17 21:42:53 +0800

    hj_10~14

---
+++ src/main/java/hf_11.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class hf_11 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        // 注意 hasNext 和 hasNextLine 的区别
+        while (in.hasNextInt()) { // 注意 while 处理多个 case
+            int a = in.nextInt();
+            String[] nums = String.valueOf(a).split("");
+            for (int i = nums.length-1; i >=0 ; i--) {
+                System.out.print(nums[i]);
+            }
+        }
+    }
+}
---
+++ src/main/java/hj_12.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class hj_12 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        // 注意 hasNext 和 hasNextLine 的区别
+        while (in.hasNext()) { // 注意 while 处理多个 case
+            String a = in.nextLine();
+            String[] nums = String.valueOf(a).split("");
+            for (int i = nums.length-1; i >=0 ; i--) {
+                System.out.print(nums[i]);
+            }
+        }
+    }
+}
