ded13ceb41a43d224428cbbe033abeb7d79f52d9
ZNW_Mac <244407031@qq.com>
2021-03-23 19:27:39 +0800

    code_62

---
+++ src/main/java/code_62.java
@@ -0,0 +1,39 @@
+public class code_62 {
+    //动态规划方法
+    public int uniquePaths(int m, int n) {
+        int [][] dp = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            dp[i][0] = 1;
+        }
+        for (int i = 0; i < n; i++) {
+            dp[0][i] = 1;
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];
+    }
+    //动态规划+滚动数组
+    public int uniquePaths_2(int m, int n) {
+        int[] dp = new int[n];
+        for (int i = 0; i < n; i++) dp[i] = 1;
+        for (int j = 1; j < m; j++) {
+            for (int i = 1; i < n; i++) {
+                dp[i] += dp[i - 1];
+            }
+        }
+        return dp[n - 1];
+    }
+
+    //数论方法
+    public int uniquePaths_1(int m, int n) {
+        int numerator = 1, denominator = 1;
+        int count = m - 1;
+        int t = m + n - 2;
+        while (count-- != 0) numerator *= (t--); // 计算分子，此时分子就会溢出
+        for (int i = 1; i <= m - 1; i++) denominator *= i; // 计算分母
+        return numerator / denominator;
+    }
+}
