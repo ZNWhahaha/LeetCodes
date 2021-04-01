73ea33be3b946d4d54a7ac4e118a35afec258b7e
ZNW_Mac <244407031@qq.com>
2021-04-01 16:24:08 +0800

    code_455,376,53

---
+++ src/main/java/code_376.java
@@ -0,0 +1,35 @@
+import sun.net.www.protocol.http.HttpURLConnection;
+
+import java.sql.Struct;
+
+public class code_376 {
+
+//    如果连续数字之间的差严格地在正数和负数之间交替，则数字序列称为摆动序列。第一个差（如果存在的话）可能是正数或负数。少于两个元素的序列也是摆动序列。
+//    例如， [1,7,4,9,2,5] 是一个摆动序列，因为差值 (6,-3,5,-7,3) 是正负交替出现的。相反, [1,4,7,2,5] 和 [1,7,4,5,5] 不是摆动序列，第一个序列是因为它的前两个差值都是正数，第二个序列是因为它的最后一个差值为零。
+//    给定一个整数序列，返回作为摆动序列的最长子序列的长度。 通过从原始序列中删除一些（也可以不删除）元素来获得子序列，剩下的元素保持其原始顺序。
+//
+//    示例 1:
+//    输入: [1,7,4,9,2,5]
+//    输出: 6
+//    解释: 整个序列均为摆动序列。
+//    示例 2:
+//    输入: [1,17,5,10,13,15,10,5,16,8]
+//    输出: 7
+//    解释: 这个序列包含几个长度为 7 摆动序列，其中一个可为[1,17,10,13,10,16,8]。
+
+    public static int wiggleMaxLength(int[] nums) {
+        if (nums.length <= 1)
+            return nums.length;
+        int nowDiff = 0;
+        int preDiff = 0;
+        int result = 1;
+        for (int i = 0; i < nums.length-1; i++) {
+            nowDiff = nums[i+1] - nums[i];
+            if ((nowDiff > 0 && preDiff <= 0) || (nowDiff < 0 && preDiff >= 0)){
+                result ++;
+                preDiff = nowDiff;
+            }
+        }
+        return  result;
+    }
+}
---
+++ src/main/java/code_53.java
@@ -0,0 +1,39 @@
+public class code_53 {
+//    给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+//    示例 1：
+//    输入：nums = [-2,1,-3,4,-1,2,1,-5,4]
+//    输出：6
+//    解释：连续子数组 [4,-1,2,1] 的和最大，为 6 。
+//    示例 2：
+//    输入：nums = [1]
+//    输出：1
+
+    //贪心算法
+    public int maxSubArray_1(int[] nums) {
+        int result = Integer.MIN_VALUE;
+        int value = 0;
+        for (int i = 0; i < nums.length; i++) {
+            value += nums[i];
+            if (value >= result)
+                result = value;
+            if (value < 0)
+                value = 0;
+        }
+        return result;
+    }
+
+    //动态规划
+    public int maxSubArray(int[] nums) {
+        if (nums.length == 0)
+            return 0;
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        int result = dp[0];
+        for (int i = 1; i < dp.length; i++) {
+            dp[i] = Math.max(dp[i-1] + nums[i],nums[i]);
+            if(dp[i] > result)
+                result = dp[i];
+        }
+        return result;
+    }
+}
