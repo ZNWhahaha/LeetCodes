f4f9fa09530243e39284a471107f08640a848336
ZNW_Mac <244407031@qq.com>
2021-05-04 22:13:40 +0800

    code_106_final

--- src/main/java/code_106.java
+++ src/main/java/code_106.java
@@ -1,3 +1,7 @@
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.LinkedList;
+
 public class code_106 {
 
 //    根据一棵树的中序遍历与后序遍历构造二叉树。
@@ -23,8 +27,58 @@ public class code_106 {
         }
     }
 
-    public TreeNode buildTree(int[] inorder, int[] postorder) {
-         
+    int[] inorder;
+    int[] postorder;
+    HashMap<Integer,Integer> inOrderIndex = new HashMap<>();
+    int postIndex;
+    
+
+    private TreeNode traversal(int leftInorder, int rightInorder) {
+        if (leftInorder > rightInorder)
+            return null;
+        int rootVal = postorder[postIndex--];
+        TreeNode root = new TreeNode(rootVal);
+        int i = inOrderIndex.get(rootVal);
+        root.right = traversal(i+1,rightInorder);
+        root.left = traversal(leftInorder,i-1);
+        return root;
+    }
+
+    public TreeNode buildTree(int[] inorder,int[] postorder){
+        this.postorder = postorder;
+        this.inorder = inorder;
+        for (int i = 0; i < inorder.length; i++) {
+            inOrderIndex.put(inorder[i],i);
+        }
+        postIndex = postorder.length-1;
+        return traversal(0,postIndex);
+    }
+
+    //迭代法
+    public TreeNode buildTree_1(int[] inorder, int[] postorder) {
+        if (postorder == null || postorder.length == 0) {
+            return null;
+        }
+        TreeNode root = new TreeNode(postorder[postorder.length - 1]);
+        Deque<TreeNode> stack = new LinkedList<TreeNode>();
+        stack.push(root);
+        int inorderIndex = inorder.length - 1;
+        for (int i = postorder.length - 2; i >= 0; i--) {
+            int postorderVal = postorder[i];
+            TreeNode node = stack.peek();
+            if (node.val != inorder[inorderIndex]) {
+                node.right = new TreeNode(postorderVal);
+                stack.push(node.right);
+            } else {
+                while (!stack.isEmpty() && stack.peek().val == inorder[inorderIndex]) {
+                    node = stack.pop();
+                    inorderIndex--;
+                }
+                node.left = new TreeNode(postorderVal);
+                stack.push(node.left);
+            }
+        }
+        return root;
     }
 
 }
