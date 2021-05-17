f51d4416f8b2a51b8be7bf32ffe2a62157e6fd15
ZNW_Mac <244407031@qq.com>
2021-05-17 15:38:37 +0800

    code_300,714,718,1143

---
+++ src/main/java/code_300.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class code_300 {
+
+//    给你一个整数数组 nums ，找到其中最长严格递增子序列的长度。
+//
+//    子序列是由数组派生而来的序列，删除（或不删除）数组中的元素而不改变其余元素的顺序。例如，[3,6,2,7] 是数组 [0,3,1,6,2,2,7] 的子序列。
+
+    public int lengthOfLIS(int[] nums) {
+        if (nums.length <= 1)
+            return nums.length;
+        int result = 0;
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp,1);
+        for (int i = 0; i < dp.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j])
+                    dp[i] = Math.max(dp[i],dp[j]+1);
+            }
+            if (dp[i] > result)
+                result = dp[i];
+        }
+        return result;
+    }
+}
--- src/main/java/code_714.java
+++ src/main/java/code_714.java
@@ -1,3 +1,5 @@
+import sun.nio.cs.ext.MacArabic;
+
 public class code_714 {
 
 //    给定一个整数数组 prices，其中第 i 个元素代表了第 i 天的股票价格 ；非负整数 fee 代表了交易股票的手续费用。
@@ -28,5 +30,17 @@ public class code_714 {
             }
         }
         return result;
+    }
+
+    public int maxProfit_1(int[] prices, int fee){
+        int len = prices.length;
+        int[][] dp = new int[len][2];
+        dp[0][0] = - prices[0];
+        for (int i = 1; i < len; i++) {
+            dp[i][0] = Math.max(dp[i-1][0],dp[i-1][1] - prices[i]);
+            dp[i][1] = Math.max(dp[i-1][1],dp[i-1][0] + prices[i] - fee);
+        }
+
+        return Math.max(dp[len-1][0],dp[len-1][1]);
     }
 }
