8eb4781269e999f696ad716febcf9db393b74e8c
ZNW_Mac <244407031@qq.com>
2021-05-19 15:18:42 +0800

    code_bianjijuli

---
+++ src/main/java/code_392.java
@@ -0,0 +1,22 @@
+public class code_392 {
+
+//    给定字符串 s 和 t ，判断 s 是否为 t 的子序列。
+//    字符串的一个子序列是原始字符串删除一些（也可以不删除）字符而不改变剩余字符相对位置形成的新字符串。（例如，"ace"是"abcde"的一个子序列，而"aec"不是）。
+//    进阶：
+//    如果有大量输入的 S，称作 S1, S2, ... , Sk 其中 k >= 10亿，你需要依次检查它们是否为 T 的子序列。在这种情况下，你会怎样改变代码？
+
+    public boolean isSubsequence(String s, String t) {
+        int[][] dp = new int[s.length()+1][t.length()+1];
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 1; j < dp[0].length; j++) {
+                if (s.charAt(i-1) == t.charAt(j-1))
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                else
+                    dp[i][j] = dp[i][j-1];
+            }
+        }
+        if (dp[s.length()][t.length()] == s.length())
+            return true;
+        return false;
+    }
+}
---
+++ src/main/java/code_583.java
@@ -0,0 +1,21 @@
+public class code_583 {
+
+//    给定两个单词 word1 和 word2，找到使得 word1 和 word2 相同所需的最小步数，每步可以删除任意一个字符串中的一个字符。
+
+    public int minDistance(String word1, String word2) {
+        int[][] dp = new int[word1.length()+1][word2.length()+1];
+        for (int i = 0; i < dp.length; i++)
+            dp[i][0] = i;
+        for (int i = 0; i < dp[0].length; i++)
+            dp[0][i] = i;
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 1; j < dp[0].length; j++) {
+                if (word1.charAt(i-1) == word2.charAt(j-1))
+                    dp[i][j] = dp[i-1][j-1];
+                else
+                    dp[i][j] = Math.min(dp[i-1][j-1]+2,Math.min(dp[i-1][j]+1,dp[i][j-1]+1));
+            }
+        }
+        return dp[word1.length()][word2.length()];
+    }
+}
