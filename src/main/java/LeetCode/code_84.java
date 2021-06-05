14513c25005d6c3dc65aadba5a4b13f23edf6534
ZNW_Mac <244407031@qq.com>
2021-06-05 21:31:45 +0800

    code

--- src/main/java/LeetCode/code_84.java
+++ src/main/java/LeetCode/code_84.java
@@ -3,8 +3,35 @@ package LeetCode;
 //给定 n 个非负整数，用来表示柱状图中各个柱子的高度。每个柱子彼此相邻，且宽度为 1 。
 //        求在该柱状图中，能够勾勒出来的矩形的最大面积。
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class code_84 {
     public int largestRectangleArea(int[] heights) {
+        int n = heights.length;
+        int[] left = new int[n];
+        int[] right = new int[n];
+        Deque<Integer> stack = new ArrayDeque<>();
 
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && heights[stack.peek()] >= heights[i])
+                stack.pop();
+            left[i] = (stack.isEmpty() ? -1 : stack.peek());
+            stack.push(i);
+        }
+
+        stack.clear();
+        for (int i = n-1; i >= 0 ; i--) {
+            while (!stack.isEmpty() && heights[stack.peek()] >= heights[i])
+                stack.pop();
+            right[i] = (stack.isEmpty() ? n : stack.peek());
+            stack.push(i);
+        }
+
+        int ans = 0;
+        for (int i = 0; i < n; ++i) {
+            ans = Math.max(ans, (right[i] - left[i] - 1) * heights[i]);
+        }
+        return ans;
     }
 }
