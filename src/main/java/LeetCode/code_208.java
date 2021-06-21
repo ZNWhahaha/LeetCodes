36c102b1fd8ecbe822ce07aec3a689135aa483eb
ZNW_Mac <244407031@qq.com>
2021-06-21 14:44:55 +0800

    Trie

---
+++ src/main/java/LeetCode/code_208.java
@@ -0,0 +1,63 @@
+package LeetCode;
+
+//Trie（发音类似 "try"）或者说 前缀树 是一种树形数据结构，用于高效地存储和检索字符串数据集中的键。这一数据结构有相当多的应用情景，例如自动补完和拼写检查。
+//        请你实现 Trie 类：
+//        Trie() 初始化前缀树对象。
+//        void insert(String word) 向前缀树中插入字符串 word 。
+//        boolean search(String word) 如果字符串 word 在前缀树中，返回 true（即，在检索之前已经插入）；否则，返回 false 。
+//        boolean startsWith(String prefix) 如果之前已经插入的字符串 word 的前缀之一为 prefix ，返回 true ；否则，返回 false 。
+
+public class code_208 {
+    class TireNode {
+        private boolean isEnd;
+        TireNode[] next;
+
+        public TireNode() {
+            isEnd = false;
+            next = new TireNode[26];
+        }
+    }
+
+    private TireNode root;
+
+    /** Initialize your data structure here. */
+    public code_208() {
+        root = new TireNode();
+    }
+
+    /** Inserts a word into the trie. */
+    public void insert(String word) {
+        TireNode node = root;
+        for (char c : word.toCharArray()) {
+            if (node.next[c - 'a'] == null) {
+                node.next[c - 'a'] = new TireNode();
+            }
+            node = node.next[c - 'a'];
+        }
+        node.isEnd = true;
+    }
+
+    /** Returns if the word is in the trie. */
+    public boolean search(String word) {
+        TireNode node = root;
+        for (char c : word.toCharArray()) {
+            node = node.next[c - 'a'];
+            if (node == null) {
+                return false;
+            }
+        }
+        return node.isEnd;
+    }
+
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    public boolean startsWith(String prefix) {
+        TireNode node = root;
+        for (char c : prefix.toCharArray()) {
+            node = node.next[c - 'a'];
+            if (node == null) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
