7b176c2d0e9c5896b75f672986ef795113f7e99f
ZNW_Mac <244407031@qq.com>
2021-06-30 15:40:36 +0800

    dp

--- src/main/java/LeetCode/code_312.java
+++ src/main/java/LeetCode/code_312.java
@@ -5,7 +5,36 @@ package LeetCode;
 //        求所能获得硬币的最大数量。
 
 public class code_312 {
+    // 动态规划五部曲
+    // 1. dp数组及下表含义
+    //      dp[i][j] 表示从数组i，到数组j（不包含i，j），戳破气球所能获得的最大金额
+    // 2. 递推公式
+    //      dp[i][j] = max(dp[i][j],dp[i][k]+dp[k][j]+nums[i]*nums[k]*nums[j])
+    // 3. dp数组初始化
+    //      dp数组全为0
+    // 4. 遍历方向
+    //      从左到右
     public int maxCoins(int[] nums) {
+        int n = nums.length;
+        int[] val = new int[n+2];
+        int[][] dp = new int[n+2][n+2];
+        val[0] = 1;
+        val[n+1] = 1;
+        for (int i = 1; i < n+1; i++) {
+            val[i] = nums[i-1];
+        }
 
+        for (int len = 3; len <= n+2; len++) {
+            for (int i = 0; i <= n+2-len; i++) {
+                int res = 0;
+                for (int k = i+1; k < i+len-1; k++) {
+                    int left = dp[i][k];
+                    int right = dp[k][i+len-1];
+                    res = Math.max(res, left + right + val[i] * val[k] * val[i+len-1]);
+                }
+                dp[i][i+len-1] = res;
+            }    
+        }
+        return dp[0][n+1];
     }
 }
