9c6bab64f5f95d120e69d58cd80a4115eac7a695
ZNW_Mac <244407031@qq.com>
2021-07-06 14:57:39 +0800

    p

---
+++ src/main/java/LeetCode/code_438.java
@@ -0,0 +1,36 @@
+package LeetCode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+//给定一个字符串 s 和一个非空字符串 p，找到 s 中所有是 p 的字母异位词的子串，返回这些子串的起始索引。
+//        字符串只包含小写英文字母，并且字符串 s 和 p 的长度都不超过 20100。
+//        说明：
+//        字母异位词指字母相同，但排列不同的字符串。
+//        不考虑答案输出的顺序。
+
+public class code_438 {
+    public List<Integer> findAnagrams(String s, String p) {
+        int n = s.length(), m = p.length();
+        List<Integer> res = new ArrayList<>();
+        if(n < m) return res;
+        int[] pCnt = new int[26];
+        int[] sCnt = new int[26];
+        for(int i = 0; i < m; i++){
+            pCnt[p.charAt(i) - 'a']++;
+            sCnt[s.charAt(i) - 'a']++;
+        }
+        if(Arrays.equals(sCnt, pCnt)){
+            res.add(0);
+        }
+        for(int i = m; i < n; i++){
+            sCnt[s.charAt(i - m) - 'a']--;
+            sCnt[s.charAt(i) - 'a']++;
+            if(Arrays.equals(sCnt, pCnt)){
+                res.add(i - m + 1);
+            }
+        }
+        return res;
+    }
+}
