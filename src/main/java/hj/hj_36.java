281d4366827d2a5ba782fe12ac98db74d3232292
ZNW_Mac <244407031@qq.com>
2021-07-08 12:44:14 +0800

    hj

--- src/main/java/hj/hj_36.java
+++ src/main/java/hj/hj_36.java
@@ -7,21 +7,47 @@ public class hj_36 {
         Scanner in = new Scanner(System.in);
         // 注意 hasNext 和 hasNextLine 的区别
         while (in.hasNext()) { // 注意 while 处理多个 case
-            String code = in.nextLine();
-            String tmp = in.nextLine();
-            int[] stand = new int[26];
-            int i;
-            for (i = 0; i < code.length(); i++) {
-                char c = code.charAt(i);
-                if (c >= 'a' && c <= 'z')
-                    stand[i] = c - 'a';
-                else if (c >= 'A' && c <= 'Z')
-                    stand[i] = c - 'A';
+            String key = in.nextLine();
+            String str = in.nextLine();
+            char[] chars = key.toLowerCase().toCharArray();
+            char[] dict = new char[26];
+            int index = 0;
+            tag1:
+            for (char ch : chars) {
+                for (int i = 0; i < index; i++) {
+                    if (ch == dict[i]) {
+                        continue tag1;
+                    }
+                }
+                dict[index] = ch;
+                index++;
             }
-            
-            for (; i < tmp.length(); i++) {
+
+            char ch = 'a';
+            tag2:
+            for (int i = 0; i < 26; i++) {
+                for (int j = 0; j < index; j++) {
+                    if (dict[j] == ch) {
+                        ch++;
+                        continue tag2;
+                    }
+                }
+                dict[index] = ch;
+                ch++;
+                index++;
+            }
+
+            char[] res = str.toCharArray();
+            for (int i = 0; i < res.length; i++) {
+                if(res[i] - 'a'>=0){
+                    res[i] = dict[res[i] - 'a'];
+                }else{
+                    res[i] = dict[res[i] - 'A'];
+                }
 
             }
+
+            System.out.println(String.valueOf(res));
         }
     }
 }
