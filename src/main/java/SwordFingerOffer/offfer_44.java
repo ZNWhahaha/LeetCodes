728b4e41fac3a33fbbd1c366fd588b9b1b10d7d0
ZNW_Mac <244407031@qq.com>
2021-08-25 14:59:38 +0800

    offer_44

---
+++ src/main/java/SwordFingerOffer/offfer_44.java
@@ -0,0 +1,44 @@
+package SwordFingerOffer;
+
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+public class offfer_44 {
+    private class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public List<Integer> largestValues(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null)
+            return result;
+        Deque<TreeNode> que = new LinkedList<>();
+        que.push(root);
+        while (!que.isEmpty()){
+            int size = que.size();
+            int max = Integer.MIN_VALUE;
+            for (int i = 0; i < size; i++) {
+                TreeNode cur = que.getFirst();
+                que.removeFirst();
+                max = Math.max(max,cur.val);
+                if (cur.left != null)
+                    que.addLast(cur.left);
+                if (cur.right != null)
+                    que.addLast(cur.right);
+            }
+            result.add(max);
+        }
+        return result;
+    }
+}
