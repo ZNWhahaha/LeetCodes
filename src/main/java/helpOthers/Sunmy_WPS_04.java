27b5044309efa28ae3351670205e5154456df1a9
ZNW_Mac <244407031@qq.com>
2021-09-10 21:04:13 +0800

    Sunmy

---
+++ src/main/java/helpOthers/Sunmy_WPS_04.java
@@ -0,0 +1,19 @@
+package helpOthers;
+
+public class Sunmy_WPS_04 {
+//    var findLucky = function(arr) {
+//        arr.sort(function(a,b){return a.localeCompare(b)});
+//        for (let str of arr) {
+//            if (getLen(arr, str) === str) return str;
+//        }
+//        return ;
+//    };
+//
+//    function getLen (arr, target) {
+//        let len = 0;
+//        for (let str of arr) {
+//            if (str === target) len++;
+//        }
+//        return len;
+//    }
+}
